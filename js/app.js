a067b4239bb3a73fae5eb08ae9e1396062060ca3
andrewplan <a.plan88@gmail.com>
2016-07-30 17:12:30 -0500

    Added YouTube player-in-lightbox functionality, fixed clickaway display, cleaned up display.

--- js/app.js
+++ js/app.js
@@ -5,15 +5,51 @@ var prevPageToken;
 //grab URLs of high-quality thumbnails from each video returned
 function showResults(results) {
     var html = "";
+    var html2 = "";
     $.each(results, function(index, value){
-        var theVideoID = value.id.videoId;
-        var thumbnailURL = value.snippet.thumbnails.high.url;
-        console.log(thumbnailURL);
-        console.log(theVideoID);
+        var theVideoIDcurrent = value.id.videoId;
+        var thumbnailURLcurrent = value.snippet.thumbnails.high.url;
+        console.log(thumbnailURLcurrent);
+        console.log(theVideoIDcurrent);
+        
         //Display thumbnail image of returned videos + Make the images clickable, leading the user to the YouTube video, on YouTube
-        html += "<a href=\'https://www.youtube.com/watch?v=" + theVideoID +"\'>" + "<img src=\'" + thumbnailURL + "\'></a>";
+        html += /* "<a href=\'https://www.youtube.com/watch?v=" + theVideoIDcurrent +"\'>" + */ "<img src=\'" + thumbnailURLcurrent + "\' value=\'" + theVideoIDcurrent + "\' onclick=\'javascript:startLightBox()\'\>";
     });
     $("#search-results").html(html);
+    
+}
+
+//code to add lightbox that will display YouTube player
+function startLightBox() {
+    $('img').click(function(event){
+        
+        //grab videoID of video
+        var theVideoIDcurrent = $(this).attr('value');
+        console.log(theVideoIDcurrent);
+        
+        //cue up lightbox container and background
+        var lbBg = document.getElementById("lightBoxBg");
+        var lb = document.getElementById("lightBox");
+        lbBg.style.display = "block";
+        lb.style.display = "block";
+        
+        //add YouTube player code to lightbox so it will display
+        var html2 = "";
+        html2 += "<iframe width=\'640\' height=\'360\' src=\'https://www.youtube.com/embed/" + theVideoIDcurrent + "?rel=0\'" + " frameborder=\'0\' allowfullscreen></iframe>"
+        $("#lightBox").html(html2);
+    });
+}
+
+function dismiss() {
+    var lbBg = document.getElementById("lightBoxBg");
+    var lb = document.getElementById("lightBox");
+    
+    //hide lightbox and lightbox background
+    lbBg.style.display = "none";
+    lb.style.display = "none";
+    
+    //upon clicking outside of lightbox, remove YouTube player code to stop video playback
+    $("#lightBox").html("");
 }
 
 //get JSON from the YouTube API based on user search term
@@ -55,24 +91,27 @@ $(function(){
         myQuery = '\"' + $("#query").val() + '\"';
         getRequest(myQuery);
         console.log(myQuery);
+        //$("#query").val("");
+        
     });
-    //Show buttons to get more results (using the previous and next page links from the JSON)
+    //Show button to get more results (using the next page links from the JSON)
     $("#more-songs").submit(function(event) {
         event.preventDefault();
         getRequest(myQuery, nextPageToken);
         $("#previous-songs").show();
     });
+    
+    //Show button to show previous results (using the previous page links from the JSON)
     $("#previous-songs").submit(function(event) {
         event.preventDefault();
         getRequest(myQuery, prevPageToken);
         if (prevPageToken === "") {
-            
+            $("#previous-songs").hide();
         }
     });
 })
 
 //Optional advanced:
-//Make the images clickable, playing them in a lightbox
 //Show a link for more from the channel that each video came from
 
 
