db037f05a59f8c5884ee2cc818f1466277912682
andrewplan <a.plan88@gmail.com>
2016-07-30 12:13:03 -0500

    Revised styling of app, finished next/prev results functionality.

--- js/app.js
+++ js/app.js
@@ -1,50 +1,78 @@
+var myQuery;
+var nextPageToken;
+var prevPageToken;
+
 //grab URLs of high-quality thumbnails from each video returned
 function showResults(results) {
     var html = "";
     $.each(results, function(index, value){
-        var theVideoIdenti = value.id.videoID;
+        var theVideoID = value.id.videoId;
         var thumbnailURL = value.snippet.thumbnails.high.url;
         console.log(thumbnailURL);
-        console.log(theVideoIdenti);
-        alert(theVideoIdenti);
-        html += "<img src=\'" + thumbnailURL + "\'>";
+        console.log(theVideoID);
+        //Display thumbnail image of returned videos + Make the images clickable, leading the user to the YouTube video, on YouTube
+        html += "<a href=\'https://www.youtube.com/watch?v=" + theVideoID +"\'>" + "<img src=\'" + thumbnailURL + "\'></a>";
     });
     $("#search-results").html(html);
 }
 
 //get JSON from the YouTube API based on user search term
-function getRequest(searchTerm) {
+function getRequest(searchTerm, userPageToken) {
     var params = {
-        part: 'snippet',
+        part: 'snippet, id', 
         key: "AIzaSyAekQLJN8I67yPvq_8hFZ68nfGlUE2lPSM",
         q: searchTerm,
-        order: 'viewCount'
+        order: 'viewCount',
+        maxResults: 6,
+        pageToken: userPageToken
     };
     url = 'https://www.googleapis.com/youtube/v3/search';
     
+    //grabs array of search results and passes it to variable userResults
     $.getJSON(url, params, function(data){
+      var data = data;
       var userResults = data.items;
+      nextPageToken = data.nextPageToken;
+        
+      if ("prevPageToken" in data) {
+          prevPageToken = data.prevPageToken;
+      }
+      else {
+          prevPageToken = "";
+          $("#previous-songs").hide();
+      }
+      
+      $("#more-songs").show();    
       showResults(userResults);
-      console.log(userResults);
+      console.log(data);
     });
 }
 
 $(function(){
-    //asks for user search term(s)
+    //asks for user search input
     $("#search-term").submit(function(event) {
         event.preventDefault();
-        var myQuery = $("#query").val();
+        myQuery = '\"' + $("#query").val() + '\"';
         getRequest(myQuery);
+        console.log(myQuery);
+    });
+    //Show buttons to get more results (using the previous and next page links from the JSON)
+    $("#more-songs").submit(function(event) {
+        event.preventDefault();
+        getRequest(myQuery, nextPageToken);
+        $("#previous-songs").show();
+    });
+    $("#previous-songs").submit(function(event) {
+        event.preventDefault();
+        getRequest(myQuery, prevPageToken);
+        if (prevPageToken === "") {
+            
+        }
     });
 })
 
-
-
-//Display thumbnail image of returned videos
-
 //Optional advanced:
-//Make the images clickable, leading the user to the YouTube video, on YouTube
 //Make the images clickable, playing them in a lightbox
 //Show a link for more from the channel that each video came from
-//Show buttons to get more results (using the previous and next page links from the JSON)
+
 
